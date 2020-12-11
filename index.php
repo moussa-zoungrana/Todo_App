b1d780cbd31a4eb13bfb0575ac8413f47ab00c0a
Moussa Zoungrana <walidzoungrana@yahoo.fr>
2020-12-11 01:49:54 +0100

    un peu de style avec css

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
+<?php require __DIR__.DIRECTORY_SEPARATOR.'elements'.DIRECTORY_SEPARATOR.'header.php' ?>
 <?php //require __DIR__.DIRECTORY_SEPARATOR.'pages'.DIRECTORY_SEPARATOR.'todo_save.php' ?>
 
 
 <h1>Todo list</h1>
+<div class="shearch">
 <form action="pages/todo_save.php" method="post">
 
-Entrez votre tache : <input type="text" name="todo" > 
-<button type="submit" name="submit">Soumettre</button>
+Enter your task : <input type="text" name="todo" > 
 
-</form>
+<button type="submit" name="submit">Add task</button>
 
+</form><br>
+</div>
 <?php require __DIR__.DIRECTORY_SEPARATOR.'pages'.DIRECTORY_SEPARATOR.'todo_print.php' ?>
+
+
+<?php require __DIR__.DIRECTORY_SEPARATOR.'elements'.DIRECTORY_SEPARATOR.'footer.php' ?>
--- pages/todo_print.php
+++ pages/todo_print.php
@@ -1,24 +1,22 @@
-<?php require dirname( __DIR__).DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.'Todo.php'; ?>
+<?php require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Todo.php'; ?>
 
-<?php  $result= Todo::recup_todo(); ?>
+<?php $result = Todo::recup_todo(); ?>
 
-<?php foreach($result as $post):?>
-
-<table>
-<tr>
-<td><?= $post->title ?></td>
-<td><?= $post->date_time ?></td>
-<td><a href="pages/todo_delete.php?del_todo=<?=$post->id?>">X</a></td>
-</tr>
-</table>
-
-<?php endforeach ?>
-
-
-<style>
-table,td{
-    border: 2px solid #6D6A69  ;
-}
-
-
-</style>
+<div class="table">
+    <table>
+        <tr>
+    <th>Task</th>
+    <th>Date</th>
+    <th>Action</th>
+        </tr>
+        <?php foreach ($result as $post) : ?>
+            <tr>
+                <div class="title">
+                    <td><?= $post->title ?></td>
+                </div>
+                <td><?= $post->date_time ?></td>
+                <td><a href="pages/todo_delete.php?del_todo=<?= $post->id ?>">X</a></td>
+            </tr>
+        <?php endforeach ?>
+    </table>
+</div>
