d0539d08104d7b1d5a02ba303fce76a1dd661382
Moussa Zoungrana <walidzoungrana@yahoo.fr>
2020-12-19 10:59:13 +0100

    style up avec css

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
-<?php require __DIR__.DIRECTORY_SEPARATOR.'elements'.DIRECTORY_SEPARATOR.'header.php' ?>
-<?php //require __DIR__.DIRECTORY_SEPARATOR.'pages'.DIRECTORY_SEPARATOR.'todo_save.php' ?>
+<?php require __DIR__ . DIRECTORY_SEPARATOR . 'elements' . DIRECTORY_SEPARATOR . 'header.php' ?>
+<?php //require __DIR__.DIRECTORY_SEPARATOR.'pages'.DIRECTORY_SEPARATOR.'todo_save.php' 
+?>
 
 
 <h1>Todo list app</h1>
 <div class="shearch">
-<form action="pages/todo_save.php" method="post">
+    <form action="pages/todo_save.php" method="post">
 
-Enter your task : <input type="text" name="todo" > 
+        <strong>Enter your task :</strong> <input type="text" name="todo">
 
-<button type="submit" name="submit">Add task</button>
+        <button type="submit" name="submit">Add task</button>
 
-</form><br>
+    </form><br>
 </div>
-<?php require __DIR__.DIRECTORY_SEPARATOR.'pages'.DIRECTORY_SEPARATOR.'todo_print.php' ?>
+<?php require __DIR__ . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR . 'todo_print.php' ?>
 
 
-<?php require __DIR__.DIRECTORY_SEPARATOR.'elements'.DIRECTORY_SEPARATOR.'footer.php' ?>
+<?php require __DIR__ . DIRECTORY_SEPARATOR . 'elements' . DIRECTORY_SEPARATOR . 'footer.php' ?>
